407039239861acbabae6c66e9f3acff50cb3bc6c
maysam-1 <maysamkh44@gmail.com>
2025-09-17 14:38:24 +0300

    added links

--- frontend/src/components/LayoutArea/Header/Header.tsx
+++ frontend/src/components/LayoutArea/Header/Header.tsx
@@ -1,15 +1,17 @@
 import React, { FC } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
-
+import logo from '../../../assets/logo.png'
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => (
   <div className="Header">
-     <nav>
-        <Link to="/">בית</Link> | <Link to="/login">התחברות</Link>
-      </nav>
-      <img src=''/>
+     <nav className="nav">
+      <Link to="/">בית</Link> |
+      <Link to="/login">התחברות</Link>
+    </nav>
+
+      <img src={logo} className="logo"/>
   </div>
 );
 
