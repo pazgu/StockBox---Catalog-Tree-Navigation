f780ca59af7e0e6abdba494ea23dba024b6a37b6
maysam-1 <maysamkh44@gmail.com>
2025-09-17 13:39:40 +0300

    added header

---
+++ frontend/src/components/LayoutArea/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './Header.css';
+
+interface HeaderProps {}
+
+const Header: FC<HeaderProps> = () => (
+  <div className="Header">
+    Header Component
+  </div>
+);
+
+export default Header;
