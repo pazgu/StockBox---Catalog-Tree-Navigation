3d7b0f3b4e7037a1b62c896694028ddb7223ecb0
yasomostafa <ym137205gmail.com>
2025-11-02 13:46:31 +0200

    fix the word נישמרו

--- frontend/src/components/Pages/ProductArea/SingleProd/SingleProd.tsx
+++ frontend/src/components/Pages/ProductArea/SingleProd/SingleProd.tsx
@@ -239,7 +239,7 @@ const SingleProd: FC<SingleProdProps> = () => {
 
   const handleSaveClick = () => {
     if (isEditing) {
-      toast.success("השינויים נישמרו בהצלחה!");
+      toast.success("השינויים נשמרו בהצלחה!");
     }
     setIsEditing(!isEditing);
   };
