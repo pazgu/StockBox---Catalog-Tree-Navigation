e63f8f723951c27b38667a4458e34c2674de18e5
Karen7-salih <Karen7-salih.salihkaren345@gmail.com>
2025-12-31 13:08:34 +0200

    add AboutBlock and UpdateAbout DTOs, and define About schema with blocks and images properties

---
+++ backend/src/about/dto/UpdateAbout.dto.ts
@@ -0,0 +1,24 @@
+import { IsArray, IsOptional, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { BlockType } from '../schemas/About.schema';
+
+export class AboutBlockDto {
+  @IsString()
+  id: string;
+
+  type: BlockType;
+
+  data: any;
+}
+
+export class UpdateAboutDto {
+  @IsOptional()
+  @IsArray()
+  @Type(() => AboutBlockDto)
+  blocks?: AboutBlockDto[];
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  images?: string[];
+}
