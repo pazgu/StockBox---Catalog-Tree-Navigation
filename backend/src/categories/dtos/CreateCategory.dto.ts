6fc076576b077834a161c5318bcc0bb1ce25c0d8
sarahmanso <sarahmansor2580@gmail.com>
2026-01-01 09:16:24 +0200

    fixed err

--- backend/src/categories/dtos/CreateCategory.dto.ts
+++ backend/src/categories/dtos/CreateCategory.dto.ts
@@ -1,12 +1,14 @@
+import { IsOptional, IsString } from 'class-validator';
+
 export class CreateCategoryDto {
   @IsOptional()
   @IsString()
   categoryName: string;
-  
+
   @IsOptional()
   @IsString()
   categoryPath: string;
-  
+
   @IsOptional()
   @IsString()
   categoryImage: string;
