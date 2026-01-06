848f6528f8c81a624a41f4fef429b48fd1af6011
maysam-1 <maysamkh44@gmail.com>
2026-01-06 14:37:34 +0200

    added models in frontend

--- frontend/src/services/ProductService.ts
+++ frontend/src/services/ProductService.ts
@@ -1,15 +1,24 @@
+import axios from "axios";
 import { environment } from "./../environments/environment.development";
+import { ProductDataDto } from "@/components/models/product.models";
 
 export interface ProductDto {
   _id?: string;
   productName: string;
-  productImage?: string; 
+  productImages?: string[];
   productDescription?: string;
   productPath: string;
-  customFields?: Record<string, any>;
+  customFields?: Array<{
+    _id?: string;
+    title: string;
+    type: 'bullets' | 'content';
+    bullets?: string[];
+    content?: string;
+  }>;
   createdAt?: string;
   updatedAt?: string;
 }
+
 
 export type CreateProductPayload = {
   productName: string;
@@ -31,29 +40,38 @@ export class ProductsService {
     return response.json();
   }
 
-  static async createProduct(payload: CreateProductPayload): Promise<ProductDto> {
-    const fd = new FormData();
-    fd.append("productName", payload.productName);
-    fd.append("productPath", payload.productPath);
+static async createProduct(payload: CreateProductPayload): Promise<ProductDto> {
+  const fd = new FormData();
+  fd.append("productName", payload.productName);
+  console.log("from service product path:",payload.productPath)
+  fd.append("productPath", payload.productPath);
 
-    if (payload.productDescription) {
-      fd.append("productDescription", payload.productDescription);
-    }
-
-    if (payload.customFields) {
-      fd.append("customFields", JSON.stringify(payload.customFields));
-    }
-
-    if (payload.imageFile) {
-        fd.append("productImageFile", payload.imageFile); 
-    }
-
-    const response = await fetch(this.baseUrl, {
-      method: "POST",
-      body: fd,
-    });
-
-    if (!response.ok) throw new Error("Failed to create product");
-    return response.json();
+  if (payload.productDescription) {
+    fd.append("productDescription", payload.productDescription);
   }
+
+  if (payload.customFields) {
+    fd.append("customFields", JSON.stringify(payload.customFields));
+  }
+
+  if (payload.imageFile) {
+    fd.append("productImageFile", payload.imageFile); // backend handles array
+  }
+
+  const response = await fetch(this.baseUrl, {
+    method: "POST",
+    body: fd,
+  });
+
+  if (!response.ok) throw new Error("Failed to create product");
+  return response.json();
+}
+
+ static async getById(id: string): Promise<ProductDataDto> {
+  const { data } = await axios.get(
+    `${environment.API_URL}/products/${id}`
+  );
+  return data;
+}
+
 }
