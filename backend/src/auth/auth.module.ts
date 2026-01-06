03da1c9934f633f939c281a5ae999af2e532f5b7
guev06 <jojamisto@gmail.com>
2026-01-06 14:02:03 +0200

    authentication and permissions guard

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from 'src/users/users.module';
-
+import { JwtStrategy } from './auth.strategy';
 @Module({
   imports: [
     ConfigModule,
@@ -23,7 +23,7 @@ import { UsersModule } from 'src/users/users.module';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
---
+++ backend/src/auth/auth.strategy.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET') || 'fallback',
+    });
+  }
+
+  validate(payload: any) {
+    console.log('JWT Strategy Validating Payload:', payload);
+    return { userId: payload.sub, role: payload.role };
+  }
+}
