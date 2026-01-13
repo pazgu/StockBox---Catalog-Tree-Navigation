7c92e8e3e32fe4e20771b4f06c6d61281885d22a
leen <leen051206@gmail.com>
2026-01-13 11:17:13 +0200

    git commit -m "add Category model to ProductsModule for validation"

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -3,12 +3,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { Product, ProductSchema } from '../schemas/Products.schema';
+import { Category, CategorySchema } from '../schemas/Categories.schema'; // Add this
 import { AuthModule } from 'src/auth/auth.module';
 import { PermissionsModule } from 'src/permissions/permissions.module';
+
 @Module({
   imports: [
     AuthModule,
-    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    MongooseModule.forFeature([
+      { name: Product.name, schema: ProductSchema },
+      { name: Category.name, schema: CategorySchema }, // Add Category model
+    ]),
     PermissionsModule,
   ],
   controllers: [ProductsController],
