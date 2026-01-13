ce3d6420375bb9a41b6819475305af93da6353ba
guev06 <jojamisto@gmail.com>
2026-01-13 15:25:52 +0200

    prettier

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { Injectable } from '@nestjs/common';
 import { Model, Types } from 'mongoose';
 import { User, FavoriteType } from 'src/schemas/Users.schema';
