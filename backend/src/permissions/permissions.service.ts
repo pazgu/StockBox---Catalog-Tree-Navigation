7d96a6c179b7f6aec666272802864d4e7c295ac8
guev06 <jojamisto@gmail.com>
2026-01-14 08:34:42 +0200

    added user pemissions objects according to group. So if a group is given permissions its users get their own permission file in the collection

--- backend/src/permissions/permissions.service.ts
+++ backend/src/permissions/permissions.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Permission } from 'src/schemas/Permissions.schema';
 import { Types } from 'mongoose';
-
+import { CreatePermissionDto } from './dto/createPermission.dto';
+import { GroupsService } from 'src/groups/groups.service';
 @Injectable()
 export class PermissionsService {
   constructor(
     @InjectModel(Permission.name) private permissionModel: Model<Permission>,
+    private groupsService: GroupsService,
   ) {}
 
   async getPermissionsForUser(allowedId: string) {
@@ -15,9 +19,25 @@ export class PermissionsService {
       .find({ allowed: new Types.ObjectId(allowedId) })
       .exec();
   }
-  async createPermission(createPermissionDto: any) {
-    const newPermission = new this.permissionModel(createPermissionDto);
-    return newPermission.save();
+
+  async createPermission(dto: CreatePermissionDto) {
+    const mainPermission = await this.permissionModel.create(dto);
+    try {
+      const group = await this.groupsService.findById(dto.allowed);
+      if (group) {
+        const memberPermissions = group.members.map((m) => ({
+          entityType: dto.entityType,
+          entityId: dto.entityId,
+          allowed: m.toString(),
+          fromGroupId: dto.allowed,
+        }));
+        await this.permissionModel.insertMany(memberPermissions);
+      }
+    } catch (e) {
+      return await this.permissionModel.create(dto);
+    }
+
+    return mainPermission;
   }
   async deletePermission(id: string) {
     return this.permissionModel.findByIdAndDelete(id).exec();
