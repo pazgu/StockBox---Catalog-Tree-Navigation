c4607cb813292d1ac3f35f0036073803e75e99b9
Karen7-salih <Karen7-salih.salihkaren345@gmail.com>
2026-01-18 12:11:30 +0200

    Added getPermissionsForGroup method and implemented getAllProducts in ProductsService

--- backend/src/permissions/permissions.controller.ts
+++ backend/src/permissions/permissions.controller.ts
@@ -34,6 +34,12 @@ export class PermissionsController {
     return { status: 'ok', deleted };
   }
 
+  @Get('by-group/:groupId')
+  @UseGuards(AuthGuard('jwt'))
+  async getPermissionsForGroup(@Param('groupId') groupId: string) {
+    return this.permissionsService.getPermissionsForAllowedId(groupId);
+  }
+
   @Get(':entityId')
   @UseGuards(AuthGuard('jwt'))
   async getPermissionsByEntityType(@Param('entityId') entityId: string) {
