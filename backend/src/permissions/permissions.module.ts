c68dc51e35784407e2a9caab4d71e7faf7ce99f3
maysam-1 <maysamkh44@gmail.com>
2026-01-18 14:23:47 +0200

    imported category

--- backend/src/permissions/permissions.module.ts
+++ backend/src/permissions/permissions.module.ts
@@ -5,13 +5,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PermissionsController } from './permissions.controller';
 import { PermissionGuard } from 'src/gaurds/permission.guard';
 import { GroupsModule } from 'src/groups/groups.module';
+import { Category, CategorySchema } from 'src/schemas/Categories.schema';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Permission.name, schema: PermissionSchema },
+      { name: Category.name, schema: CategorySchema }
     ]),
     GroupsModule,
+    UsersModule,
   ],
   providers: [PermissionsService, PermissionGuard],
   controllers: [PermissionsController],
