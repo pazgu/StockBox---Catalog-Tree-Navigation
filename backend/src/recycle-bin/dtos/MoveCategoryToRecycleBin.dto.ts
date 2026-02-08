eed502e952225307181011fbbae8d765a3a92bb1
leen <leen051206@gmail.com>
2026-02-08 09:13:11 +0200

    created dtos for deleteing and restoring - items from recycle bin

---
+++ backend/src/recycle-bin/dtos/MoveCategoryToRecycleBin.dto.ts
@@ -0,0 +1,7 @@
+import { IsString, IsEnum, IsOptional } from 'class-validator';
+
+export class MoveCategoryToRecycleBinDto {
+  @IsOptional()
+  @IsEnum(['cascade', 'move_up'])
+  strategy?: 'cascade' | 'move_up';
+}
