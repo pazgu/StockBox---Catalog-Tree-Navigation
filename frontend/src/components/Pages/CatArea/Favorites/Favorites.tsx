1cc7bcedd0ba8532aac11d82e0315ec2dddfaff0
guev06 <jojamisto@gmail.com>
2026-02-22 10:49:20 +0200

    fixed favorites toasts

--- frontend/src/components/Pages/CatArea/Favorites/Favorites.tsx
+++ frontend/src/components/Pages/CatArea/Favorites/Favorites.tsx
@@ -122,7 +122,7 @@ export const Favorites: React.FC = () => {
     if (isFavorite) {
       setFavorites((prev) => prev.filter((fav) => fav.id !== categoryId));
       setCategories((prev) => prev.filter((c) => c._id !== categoryId));
-      toast.info(`${categoryName} הוסר מהמועדפים`);
+      toast.info(`${categoryName} הוסרה מהמועדפים`);
       return;
     }
 
