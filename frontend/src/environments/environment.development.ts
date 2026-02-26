f2bb31200c14bf1a1214f3cadd59e7b68ff1bffd
Karen7-salih <Karen7-salih.salihkaren345@gmail.com>
2026-02-26 12:11:45 +0200

    added the url

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: false,
   API_URL: "http://localhost:4000",
+    DEFAULT_PRODUCT_IMAGE_URL:
+    "https://res.cloudinary.com/dbludqv2z/image/upload/v1771754267/default_product_nsbhee.svg",
+  DEFAULT_CATEGORY_IMAGE_URL:
+    "https://res.cloudinary.com/dbludqv2z/image/upload/v1771754267/default_category_wqlm19.svg",
 };
