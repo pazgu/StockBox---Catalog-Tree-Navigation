df5d6cec91d59cbf0286c7d577f556059f87fae5
maysam-1 <maysamkh44@gmail.com>
2026-03-21 23:16:28 +0200

    initilize previousPath from localStorage instead of doing it in useEffect

--- frontend/src/context/PathContext.tsx
+++ frontend/src/context/PathContext.tsx
@@ -8,24 +8,22 @@ interface PathContextType {
 const PathContext = createContext<PathContextType | undefined>(undefined);
 
 export const PathProvider = ({ children }: { children: ReactNode }) => {
-  const [previousPath, setPreviousPathState] = useState<string | null>(null);
-
-  useEffect(() => {
-    const saved = localStorage.getItem("previousPath");
-    if (saved) setPreviousPathState(saved);
-  }, []);
+  const [previousPath, setPreviousPathState] = useState<string | null>(() => {
+    return localStorage.getItem("previousPath");
+  });
+ 
 
   const dynamicPages = [
-  /^\/products\/[^/]+$/,          
-  /^\/permissions\/[^/]+\/[^/]+$/, 
-  /^\/permissions\/product\/[^/]+$/, 
-  /^\/permissions\/category\/[^/]+$/ 
-];
+    /^\/products\/[^/]+$/,
+    /^\/permissions\/[^/]+\/[^/]+$/,
+    /^\/permissions\/product\/[^/]+$/,
+    /^\/permissions\/category\/[^/]+$/
+  ];
 
   const setPreviousPath = (path: string | null) => {
     if (path) {
       const isDynamic = dynamicPages.some((pattern) => pattern.test(path));
-      if (isDynamic) return; 
+      if (isDynamic) return;
 
       setPreviousPathState(path);
       localStorage.setItem("previousPath", path);
