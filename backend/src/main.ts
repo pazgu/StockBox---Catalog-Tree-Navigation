4d92908cf3875c5ea985fe39a1fac625bc484562
maysam-1 <maysamkh44@gmail.com>
2026-03-29 12:02:17 +0300

    docker compose

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? 4000, '0.0.0.0');
 }
 bootstrap();
